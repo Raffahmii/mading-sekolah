43fa4009cc1459a7b708ecc2ecfcb84d21ae06c2
Raffahmii <mobilepremiumleugue@gmail.com>
2026-04-12 11:32:32 +0700

    njay

--- config/db.php
+++ config/db.php
@@ -1,13 +1,15 @@
 <?php
-$conn = pg_connect(
-    "host=" . getenv("PGHOST") .
-    " port=" . getenv("PGPORT") .
-    " dbname=" . getenv("PGDATABASE") .
-    " user=" . getenv("PGUSER") .
-    " password=" . getenv("PGPASSWORD")
-);
+try {
+    $conn = new PDO(
+        "pgsql:host=" . getenv("PGHOST") . 
+        ";port=" . getenv("PGPORT") . 
+        ";dbname=" . getenv("PGDATABASE"),
+        getenv("PGUSER"),
+        getenv("PGPASSWORD")
+    );
 
-if (!$conn) {
-    echo "Koneksi gagal";
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    echo "Koneksi gagal: " . $e->getMessage();
 }
-?>
